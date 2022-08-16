9e744abd3b3dc5c56c9c6b514c19bb854ce930a6
abdallahmontaser <73659030+abdallahmontaser@users.noreply.github.com>
2022-08-16 17:03:33 +0200

    Create M. Marching into the futre.cpp

---
+++ M. Marching into the futre.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+ll lcm(ll a, ll b) { return (b / gcd(a, b)) * a; }
+
+void Candidate_Elde7k() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+}
+
+int main() {
+    Candidate_Elde7k();
+    int T; cin >> T;
+    while (T--) {
+        int n; cin >> n;
+        vector<ll>A(n), B(n), C(n);
+        for (auto& it : A) cin >> it;
+        for (auto& it : B) cin >> it;
+        for (auto& it : C) cin >> it;
+        ll mn = INT_MAX;
+        int idx = -1;
+        for (int i = 0; i < n; i++) {
+            if (A[i] + B[i] + C[i] < mn) {
+                mn = A[i] + B[i] + C[i];
+                idx = i + 1;
+            }
+        }
+        cout << mn << " " << idx << "\n";
+    }
+    return 0;
+}
